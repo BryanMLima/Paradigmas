6d4ebabc6192dcd85b3fe42cb9959b94f8219cb3
Bryan Lima <bryan.lima@hotmail.com>
2019-09-30 12:29:00 -0300

    update 8c

--- Atividade IX/problema8c.hs
+++ Atividade IX/problema8c.hs
@@ -8,6 +8,10 @@ verifyNumber (a:b) i | (a == i) = (verifyNumber b i) + 1
 verify :: [Int] -> Int -> Bool
 verify a b | (verifyNumber a b) > 1 = False
            | otherwise = True
+
+verify_all :: [Int] -> Int -> Bool
+verify_all a n =
+    [1..n*2]
           
 main = do
     let b = (fromLists [[2, 7, 6],
@@ -15,6 +19,6 @@ main = do
              [4, 3, 8]])
 
     let n = (nrows b)
-    print (all (verify (toList b) 1) [1..n*2])
+    -- print (all (verify (toList b) 1) [1..n*2])
     -- print(all (verify (toList b) 1) [1..n**2])
     -- print(fromLists b)
