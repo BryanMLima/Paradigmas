081dfd66a2476b052e0a5214d61492edb00557e1
Bryan Lima <bryan.lima@hotmail.com>
2019-10-15 21:06:20 -0300

    adicionado simulado 2

--- Simulado II/palindrome.lisp
+++ Simulado II/palindrome.lisp
@@ -1,17 +1,30 @@
-;; (defun palindrome (n)
-;;     (= n (reverso n))
-;; )
+(defun palindrome (n)
+    (= n (reverso n))
+)
 
 (defun reverso (lista)
     (if (null lista)
         ()
-        (concatenate 'list (reverso (cdr lista)) (car lista))
+        (list* (reverso (cdr lista)) (car lista))
+    )
+)
+
+;; (defun reverso (reversa lista)
+;;     (if (null lista)
+;;         reversa
+;;         (reverso (list* (car lista) reversa ) (cdr lista))
+;;     )
+;; )
+
+(defun compare (lista1 lista2)
+    (if (or (null lista1) (null lista2))
+    
     )
 )
 
 (defun main()
-    ;; (write-line (write-to-string (palindrome (list 1 2 3 3 2 1))))
-    (write-line (write-to-string (reverso (list 2 2 3 3 2 1))))
+    (write-line (write-to-string (palindrome (list 1 2 3 3 2 1))))
+    ;; (write-line (write-to-string (reverso (list 2 2 3 3 2 1))))
 )
 
 (main)
